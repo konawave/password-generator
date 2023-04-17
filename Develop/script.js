dfb678b189fe417c2736f3c8b9a4a34b58df0301
konawave <nickkjeffs@gmail.com>
2023-04-17 14:44:26 -0400

    I am struggling, no major changes thus far

--- Develop/script.js
+++ Develop/script.js
@@ -1,13 +1,13 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
-
 // Write password to the #password input
+var pwlength = 0
+
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
 }
 
 // Add event listener to generate button
