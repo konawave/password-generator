9019eae1b6262337e77ce6a70ce1646db3df8880
konawave <nickkjeffs@gmail.com>
2023-04-19 13:29:47 -0400

    no changes made, comitting to merge

--- Develop/script.js
+++ Develop/script.js
@@ -1,7 +1,6 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 // Write password to the #password input
-var pwlength = 0
 
 function writePassword() {
   var password = generatePassword();
@@ -12,3 +11,11 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+// Create a variable called generateBtn that is = to a querySelector that returns the #generate id, which is the ID given to the button.
+
+// Function called writePassword that, when called, provides two variables: one called password that is = to generatePassword();, and a second called passwordText that is = to document.querySelector (which returns the value of anything with a password ID, the text area)
+
+// IDK what passwordText.value = password means. 
+
+// Element that adds an event listener to the generate button, 
