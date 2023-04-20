37779ce54f8f0bbe5124e9489a7c617d4fcef967
konawave <nickkjeffs@gmail.com>
2023-04-20 15:47:59 -0400

    Refined the If function, however having trouble translating to text box

--- Develop/script.js
+++ Develop/script.js
@@ -3,11 +3,6 @@
   // Variables/Objects: 
     
 var password ="" ;// blank variable for the password generation
-var play = false;  // boolean for PLAY
-var uppercase = false; // boolean for uppercase
-var lowercase = false;  // boolean for lowercase
-var numbers = false; // boolean for numbers
-var special = false; // boolean for special characters
 var pwLength = 0; // blank variable for length 
 var validAnswers = []; // An array with all valid answers, potentially has to be separate arrays for each category
 var validUpper = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
@@ -31,46 +26,54 @@ var generateBtn = document.querySelector("#generate");
     // prints generated password into the text box on the webpage.
   // This function may need to be moved above letsPlay function since letsPlay calls generatePassword.
 let confirmUpper = confirm("Do you want uppercase letters?");
-if (confirmUpper == true) {
-  uppercase = true;
-  return
-}
 let confirmLower = confirm("Do you want lowercase letters?");
 let confirmNumbers = confirm("Do you want numbers?");
 let confirmSpecial = confirm("Do you want special characters?");
+let passwordLength = prompt("How long do you want your password? Pick a length between 8 and 128 characters.")
 
-if (uppercase = true) {
-      validAnswers.concat(validUpper);
-      }
-console.log(validAnswers)
-// function generatePassword() {
-//   // confirmUpper;
-//   if (confirmUpper = true) {
-//       validAnswers.concat(validUpper);
-//     }
+function generatePassword() {
+  // confirmUpper;
+  if (confirmUpper = true) {
+      validAnswers = validAnswers.concat(validUpper);
+    }
     
-//     // confirmLower;
-//   if (confirmLower = true) {
-//       validAnswers.concat(validLower);
-//     }
+    // confirmLower;
+  if (confirmLower = true) {
+    validAnswers = validAnswers.concat(validLower);
+    }
 
-//     // confirmNumbers;
-//   if (confirmNumbers = true) {
-//       validAnswers.concat(validNumbers);
-//     }
+    // confirmNumbers;
+  if (confirmNumbers = true) {
+    validAnswers = validAnswers.concat(validNumbers);
+    }
 
-//     // confirmSpecial;
-//   if (confirmSpecial = true) {
-//       validAnswers.concat(validSpecial)
-//     }
+    // confirmSpecial;
+  if (confirmSpecial = true) {
+    validAnswers = validAnswers.concat(validSpecial);
+    }
 
-//   if ((confirmSpecial && confirmNumbers && confirmUpper && confirmLower) == false) {
-//       alert("At least one option needs to be chosen! Please try again!");
-//       generatePassword;
-//     }
-//   }
+  // if ((confirmSpecial && confirmNumbers && confirmUpper && confirmLower) == false) {
+  //     alert("At least one option needs to be chosen! Please try again!")
+  //     generatePassword;
+    // if (confirmSpecial == false && confirmUpper == false && confirmNumbers == false && confirmLower == false) {
+    //   alert("Try again!")
+    //   generatePassword
+    // }
+    // }
+  if (passwordLength >= 8 && passwordLength <= 128) {
+    for (i = 0; i < pwLength; i++ ) {
+      var password = Math.floor(Math.random() * validAnswers.length);
+  }
 
-// generatePassword()
+    }
+    else {
+      alert("Please only use numbers.");
+      generatePassword();
+    }
+  return password;
+  }
+
+generatePassword();
 
 function writePassword() {
   var password = generatePassword(); // password variable calls generatePassword function.
