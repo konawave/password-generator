2b6aa5c50cf9a24f7eba762a3cd830c5f6d364c0
konawave <nickkjeffs@gmail.com>
2023-04-20 14:19:39 -0400

    Saving now before I make a branch to try restarting - having too many issues concatenating arrays

--- Develop/script.js
+++ Develop/script.js
@@ -3,22 +3,21 @@
   // Variables/Objects: 
     
 var password ="" ;// blank variable for the password generation
-var play = true;  // boolean for PLAY
-var uppercase = true; // boolean for uppercase
-var lowercase = true;  // boolean for lowercase
-var numbers = true; // boolean for numbers
-var special = true; // boolean for special characters
+var play = false;  // boolean for PLAY
+var uppercase = false; // boolean for uppercase
+var lowercase = false;  // boolean for lowercase
+var numbers = false; // boolean for numbers
+var special = false; // boolean for special characters
 var pwLength = 0; // blank variable for length 
 var validAnswers = []; // An array with all valid answers, potentially has to be separate arrays for each category
-var validUpper = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
-
-var validLower = validUpper.lowercase
+var validUpper = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+var validLower = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p"]
+var validSpecial = ["!", "@", "#", "$", "%", "^", "&", "*"];
+var validNumbers = ["0", "1", "2", "3", "4", "5", "5", "6", "7", "8", "9"];
 var generateBtn = document.querySelector("#generate");
 // Write password to the #password input
 
-// A function that:
-  // Asks the user if they want to play [CONFIRM]
-// Another Function that:
+// Function that:
   // Asks user if they want uppercase [CONFIRM]
   // Asks user if they want lowercase [CONFIRM]
   // Asks user if they want numbers [CONFIRM]
@@ -31,9 +30,47 @@ var generateBtn = document.querySelector("#generate");
     // function makes blank password variable equal to Math.floor((Math.random) * (array.length)), possibly a for loop to iterate through each character of the password?
     // prints generated password into the text box on the webpage.
   // This function may need to be moved above letsPlay function since letsPlay calls generatePassword.
-confirm("Would you like uppercase characters?")
-validAnswers.push(validUpper)
+let confirmUpper = confirm("Do you want uppercase letters?");
+if (confirmUpper == true) {
+  uppercase = true;
+  return
+}
+let confirmLower = confirm("Do you want lowercase letters?");
+let confirmNumbers = confirm("Do you want numbers?");
+let confirmSpecial = confirm("Do you want special characters?");
+
+if (uppercase = true) {
+      validAnswers.concat(validUpper);
+      }
 console.log(validAnswers)
+// function generatePassword() {
+//   // confirmUpper;
+//   if (confirmUpper = true) {
+//       validAnswers.concat(validUpper);
+//     }
+    
+//     // confirmLower;
+//   if (confirmLower = true) {
+//       validAnswers.concat(validLower);
+//     }
+
+//     // confirmNumbers;
+//   if (confirmNumbers = true) {
+//       validAnswers.concat(validNumbers);
+//     }
+
+//     // confirmSpecial;
+//   if (confirmSpecial = true) {
+//       validAnswers.concat(validSpecial)
+//     }
+
+//   if ((confirmSpecial && confirmNumbers && confirmUpper && confirmLower) == false) {
+//       alert("At least one option needs to be chosen! Please try again!");
+//       generatePassword;
+//     }
+//   }
+
+// generatePassword()
 
 function writePassword() {
   var password = generatePassword(); // password variable calls generatePassword function.
