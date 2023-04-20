39354e5b9e0cf436472f3bf89977b3de09174261
konawave <nickkjeffs@gmail.com>
2023-04-20 17:08:13 -0400

    successfully got it to print in the password text box, but it is not taking the length into account. Will adjust with For loop.

--- Develop/script.js
+++ Develop/script.js
@@ -1,53 +1,73 @@
 // Assignment Code
-// As a user, I need a secure password generator that takes into account criteria such as characters, uppercase, lowercase, and numbers.
-  // Variables/Objects: 
     
-var password ="" ;// blank variable for the password generation
-var play = false;  // boolean for PLAY
-var uppercase = false; // boolean for uppercase
-var lowercase = false;  // boolean for lowercase
-var numbers = false; // boolean for numbers
-var special = false; // boolean for special characters
-var pwLength = 0; // blank variable for length 
-var validAnswers = []; // An array with all valid answers, potentially has to be separate arrays for each category
-var validUpper = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
-var validLower = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p"]
-var validSpecial = ["!", "@", "#", "$", "%", "^", "&", "*"];
-var validNumbers = ["0", "1", "2", "3", "4", "5", "5", "6", "7", "8", "9"];
+  var passwordtext ="" ;// blank variable for the password generation
+  var validAnswers = []; // An array with all valid answers, potentially has to be separate arrays for each category
+ 
+  var generateBtn = document.querySelector("#generate");
+  // Write password to the #password input
+  
+  let confirmUpper = confirm("Do you want uppercase letters?");
+  let confirmLower = confirm("Do you want lowercase letters?");
+  let confirmNumbers = confirm("Do you want numbers?");
+  let confirmSpecial = confirm("Do you want special characters?");
+  let passwordLength = prompt("How long do you want your password? Pick a length between 8 and 128 characters.");
+  
+  function generatePassword() {
+    var validUpper = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+    var validLower = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p"]
+    var validSpecial = ["!", "@", "#", "$", "%", "^", "&", "*"];
+    var validNumbers = ["0", "1", "2", "3", "4", "5", "5", "6", "7", "8", "9"];
 
+    if (confirmUpper = true) {
+        validAnswers = validAnswers.concat(validUpper);
+      }
+      
+    if (confirmLower = true) {
+      validAnswers = validAnswers.concat(validLower);
+      }
+  
+    if (confirmNumbers = true) {
+      validAnswers = validAnswers.concat(validNumbers);
+      }
+  
+    if (confirmSpecial = true) {
+      validAnswers = validAnswers.concat(validSpecial);
+      }
+    
+    passwordtext = validAnswers[Math.floor(Math.random() * validAnswers.length)];
 
-var generateBtn = document.querySelector("#generate");
-// Write password to the #password input
-
-function writePassword() {
-  var password = generatePassword(); // password variable calls generatePassword function.
-  var passwordText = document.querySelector("#password");
-
-  passwordText.value = password;
-}
-
-// *Add event listener to generate button
-generateBtn.addEventListener("click", writePassword); // this CALLS function writePassword
-
-
-// [CONFIRM] If they want uppercase
-  // If return true, concat validAnswers and validUpper
-validAnswers = validAnswers.concat(validUpper)
-validAnswers = validAnswers.concat(validLower)
-console.log(validAnswers)
-// [CONFIRM] If they want lowecase
-// [CONFIRM] If they want special characters
-// [CONFIRM] If they want numbers
-
-// Function that:
-  // Asks user if they want uppercase [CONFIRM]
-  // Asks user if they want lowercase [CONFIRM]
-  // Asks user if they want numbers [CONFIRM]
-  // Asks user if they want special characters [CONFIRM]
-  // Asks user how long they want their password (between 8 and 128 characters) [PROMPT]
-  // Validate choices to ensure at least one option is a YES. 
-    // If not, [ALERT] that they need to check at least one option
-    // Re-runs function
-  // If selection is valid;
-    // function makes blank password variable equal to Math.floor((Math.random) * (array.length)), possibly a for loop to iterate through each character of the password?
-
+    return passwordtext
+  }
+    // if ((confirmSpecial && confirmNumbers && confirmUpper && confirmLower) == false) {
+    //     alert("At least one option needs to be chosen! Please try again!")
+    //     generatePassword;
+      // if (confirmSpecial == false && confirmUpper == false && confirmNumbers == false && confirmLower == false) {
+      //   alert("Try again!")
+      //   generatePassword
+      // }
+      // }
+    // if (passwordLength >= 8 && passwordLength <= 128) {
+    //   for (i = 0; i < passwordLength; i++ ) {
+    //     passwordunit = Math.floor(Math.random() * validAnswers.length);
+    //     passwordtext = passwordtext.append(passwordunit);
+    // }
+  
+    //   }
+    //   else {
+    //     alert("Please only use numbers.");
+    //     generatePassword();
+    //   }
+    // return
+  
+  
+  function writePassword() {
+    var password = generatePassword(); // password equals the value returned in the generatePassword function
+    var passwordText = document.querySelector("#password"); // passwordText is equal to the password text box
+  
+    passwordText.value = password; // makes password equal to the primitive value of passwordText.
+  }
+  
+  // *Add event listener to generate button
+  generateBtn.addEventListener("click", writePassword); // this CALLS function writePassword
+  
+  
